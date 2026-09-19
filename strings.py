9c2655f7242bacd064e23c0e7a58420b9e31622e
smileyharini424-cloud <smileyharini424@gmail.com>
2026-09-18 21:48:10 -0700

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,12 @@
+text = input("Enter a string: ")
+
+print("String:", text)
+
+if len(text) > 0:
+    print("First Character:", text[0])
+
+print("Sliced String:", text[:3])
+
+print("Concatenation:", text + " Programming")
+
+print("Repetition:", text * 2)
